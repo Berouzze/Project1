2d3f9906a8a94d93ba40f280c5bfd55a32ce875a
berouze <berouze@bk.ru>
2023-02-02 15:29:30 +0300

    New file name

--- Multiplication.go
+++ Multiplication.go
@@ -5,7 +5,7 @@ import "fmt"
 func main() {
 	var numbers int
 	var maxNumber int = 0
-	var multiplication int
+	var multiplication int = 1
 	for c := 0; c < 3; c++ {
 		fmt.Scan(&numbers) // Введите свои числа
 		if numbers > maxNumber {
@@ -14,7 +14,7 @@ func main() {
 	}
 	fmt.Println(maxNumber) // Вывод максимального числа
 
-	for c := 1; c < maxNumber; c++ {
+	for c := 1; c <= maxNumber; c++ {
 		multiplication *= c
 	}
 
