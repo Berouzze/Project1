4c6428a36c22a180900dba00a6385ccae08a7596
berouze <berouze@bk.ru>
2023-02-02 15:33:56 +0300

    Новые комменты к задаче

--- Multiplication.go
+++ Multiplication.go
@@ -6,17 +6,17 @@ func main() {
 	var numbers int
 	var maxNumber int = 0
 	var multiplication int = 1
+
 	for c := 0; c < 3; c++ {
-		fmt.Scan(&numbers) // Введите свои числа
+		fmt.Scan("Введите 3 своих числа", &numbers) // Введите свои числа
 		if numbers > maxNumber {
 			maxNumber = numbers
 		}
 	}
-	fmt.Println(maxNumber) // Вывод максимального числа
 
 	for c := 1; c <= maxNumber; c++ {
 		multiplication *= c
 	}
 
-	fmt.Print(multiplication)
+	fmt.Print("Ответ ", multiplication) // Ответ
 }
