4fc0fd4a4faf774ec8c3e4eb6ce3d613a8dc5a8a
berouze <berouze@bk.ru>
2023-02-05 12:38:37 +0300

    Новые правки в задачу

--- Multiplication.go
+++ Multiplication.go
@@ -6,10 +6,12 @@ func main() {
 	var numbers int
 	var maxNumber int = 0
 	var multiplication int = 1
+	var amount int
+	fmt.Print("Введите желаемое количество чисел: ")
+	fmt.Scan(&amount)
+	fmt.Print("Введите свои числа: ")
 
-	fmt.Print("Введите 3 своих числа:")
-
-	for c := 0; c < 3; c++ {
+	for c := 0; c < amount; c++ {
 		fmt.Scan(&numbers) // Введите свои числа
 		if numbers > maxNumber {
 			maxNumber = numbers
@@ -20,5 +22,5 @@ func main() {
 		multiplication *= c
 	}
 
-	fmt.Print("Ответ ", multiplication) // Ответ
+	fmt.Print(multiplication) // Ответ
 }
