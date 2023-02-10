b8b063890d3865cfc7118b0905192cb15d92dc3e
berouze <berouze@bk.ru>
2023-02-10 17:58:32 +0300

    Second home task complete

--- MyApp/Task_1/HomeTasks/HomeWorkOne.cs
+++ MyApp/Task_1/HomeTasks/HomeWorkOne.cs
@@ -1,6 +1,6 @@
 // Задача 2: Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
 
-/*Console.WriteLine("Введите целые числа");
+/* Console.WriteLine("Введите целые числа");
 Console.WriteLine("Первое число: ");
 int number_one = Convert.ToInt32(Console.ReadLine());
 Console.WriteLine("Второе число: ");
@@ -10,5 +10,22 @@ if (number_one > number_two) {
 } else {
      Console.WriteLine("Число "+ number_two + " больше числа "+ number_one);
 }
+*/
+
+// Задача 4: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+
+/*
+Console.WriteLine("Введите три целых числа");
+int cycle = 3;
+int size = 0;
+int maxNumber = 0;
+while (size<cycle) {
+int numbers = Convert.ToInt32(Console.ReadLine());
+if (numbers> maxNumber) {
+    maxNumber = numbers;
+} 
+size++;
+}
+Console.WriteLine("Максимальное число " + maxNumber);
 */
 
