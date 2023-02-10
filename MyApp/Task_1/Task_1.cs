74208f02bd66f16e83cbcc509ad0a13aca2fe72b
berouze <berouze@bk.ru>
2023-02-10 17:21:46 +0300

    C# First Task

---
+++ MyApp/Task_1/Task_1.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Введите числа");
+Console.WriteLine("Число a: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Число b: ");
+int b = Convert.ToInt32(Console.ReadLine());
+if (a == b * b) {
+    Console.WriteLine("true");
+} else {
+    Console.WriteLine("false");
+}
