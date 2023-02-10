4fd146aa52e6db81899f3079507f694b883e5d8b
berouze <berouze@bk.ru>
2023-02-10 18:05:50 +0300

    Third home task complete

--- MyApp/Task_1/HomeTasks/HomeWorkOne.cs
+++ MyApp/Task_1/HomeTasks/HomeWorkOne.cs
@@ -29,3 +29,17 @@ size++;
 Console.WriteLine("Максимальное число " + maxNumber);
 */
 
+// Задача 6: Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка)
+
+/*
+Console.WriteLine("Введите целое число");
+
+int number_one = Convert.ToInt32(Console.ReadLine());
+
+if (number_one%2==0) {
+    Console.WriteLine(number_one + " четное число.");
+} else {
+    Console.WriteLine(number_one +" нечетное число.");
+}
+*/
+
