b64c6021e425b1f0e4f715af7beb7d257e4e02b4
berouze <berouze@bk.ru>
2023-02-10 18:18:50 +0300

    АFourthА home task complete

--- MyApp/Task_1/HomeTasks/HomeWorkOne.cs
+++ MyApp/Task_1/HomeTasks/HomeWorkOne.cs
@@ -43,3 +43,16 @@ if (number_one%2==0) {
 }
 */
 
+// Задача 8: Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N
+
+Console.WriteLine("Введите целое число");
+
+int number_one = Convert.ToInt32(Console.ReadLine());
+int size = 1;
+
+while (size <= number_one) {    // вывод числа  включая number_one
+   if (size%2==0) { 
+    Console.WriteLine(size); 
+}
+size++;
+}
