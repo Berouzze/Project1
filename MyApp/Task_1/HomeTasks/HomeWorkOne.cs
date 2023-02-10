ce76ffbee3e5b08259fddf984ab53133a06d1d20
berouze <berouze@bk.ru>
2023-02-10 17:47:43 +0300

    First home task complete

---
+++ MyApp/Task_1/HomeTasks/HomeWorkOne.cs
@@ -0,0 +1,14 @@
+// Задача 2: Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+
+/*Console.WriteLine("Введите целые числа");
+Console.WriteLine("Первое число: ");
+int number_one = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Второе число: ");
+int number_two = Convert.ToInt32(Console.ReadLine());
+if (number_one > number_two) {
+    Console.WriteLine("Число "+number_one+ " больше числа "+ number_two);
+} else {
+     Console.WriteLine("Число "+ number_two + " больше числа "+ number_one);
+}
+*/
+
